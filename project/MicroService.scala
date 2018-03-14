37e6609e3d79ceaca9e94c145e9f414be74f66c4
Tomasz Rosiek <tomasz.rosiek@digital.hmrc.gov.uk>
2018-03-14 10:38:14 +0000

    Fixed SBT configuration to work correctly in open build environment

--- project/MicroService.scala
+++ project/MicroService.scala
@@ -2,35 +2,48 @@ import sbt.Keys._
 import sbt.Tests.{SubProcess, Group}
 import sbt._
 import play.routes.compiler.StaticRoutesGenerator
-import uk.gov.hmrc.sbtdistributables.SbtDistributablesPlugin._
+import play.sbt.routes.RoutesKeys.routesGenerator
 
 
 trait MicroService {
 
   import uk.gov.hmrc._
   import DefaultBuildSettings._
-  import uk.gov.hmrc.ShellPrompt
-  import play.sbt.routes.RoutesKeys.routesGenerator
+  import TestPhases._
+  import scoverage.ScoverageKeys
+  import uk.gov.hmrc.sbtdistributables.SbtDistributablesPlugin._
 
   import TestPhases._
 
   val appName: String
-  val appVersion: String
 
-  lazy val appDependencies : Seq[ModuleID] = ???
-  lazy val plugins : Seq[Plugins] = Seq(play.sbt.PlayScala)
-  lazy val playSettings : Seq[Setting[_]] = Seq.empty
+  lazy val appDependencies: Seq[ModuleID] = ???
+  lazy val plugins: Seq[Plugins]          = Nil
+  lazy val playSettings: Seq[Setting[_]]  = Seq.empty
+
+  lazy val scoverageSettings = {
+    Seq(
+      // Semicolon-separated list of regexs matching classes to exclude
+      ScoverageKeys.coverageExcludedPackages := "<empty>;Reverse.*;.*AuthService.*;models/.data/..*;view.*",
+      ScoverageKeys.coverageExcludedFiles :=
+        ".*/frontendGlobal.*;.*/frontendAppConfig.*;.*/frontendWiring.*;.*/views/.*_template.*;.*/govuk_wrapper.*;.*/routes_routing.*;.*/BuildInfo.*",
+      // Minimum is deliberately low to avoid failures initially - please increase as we add more coverage
+      ScoverageKeys.coverageMinimum := 25,
+      ScoverageKeys.coverageFailOnMinimum := false,
+      ScoverageKeys.coverageHighlighting := true,
+      parallelExecution in Test := false
+    )
+  }
 
   lazy val microservice = Project(appName, file("."))
-    .enablePlugins(plugins : _*)
-    .settings(playSettings : _*)
-    .settings(version := appVersion)
-    .settings(scalaSettings: _*)
+    .enablePlugins(Seq(play.sbt.PlayScala) ++ plugins: _*)
+    .settings(playSettings: _*)
+    .settings(scalaSettings ++ scoverageSettings: _*)
     .settings(publishingSettings: _*)
     .settings(defaultSettings(): _*)
     .settings(
       targetJvm := "jvm-1.8",
-      shellPrompt := ShellPrompt(appVersion),
+      scalaVersion := "2.11.12",
       libraryDependencies ++= appDependencies,
       parallelExecution in Test := false,
       fork in Test := false,
