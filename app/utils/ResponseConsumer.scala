1c34a0abed9c57c05a39f741a74e9f882a8cfe96
Tomasz Rosiek <tomasz.rosiek@digital.hmrc.gov.uk>
2018-05-02 10:41:32 +0100

    PLATY-532 Peformance optimization regarding adding notifications to the list

--- app/utils/ResponseConsumer.scala
+++ app/utils/ResponseConsumer.scala
@@ -13,14 +13,15 @@ trait ResponseConsumer {
   def retrieveResponses: ResponseLog
 }
 
-class InMemoryResponseConsumer(private var responsesDate: LocalDate,
-                               private var responses: Seq[JsValue]) extends ResponseConsumer {
+class InMemoryResponseConsumer(private var responsesDate: LocalDate) extends ResponseConsumer {
+
+  private var responses: List[JsValue] = Nil
 
   override def addResponse(response: JsValue, today: LocalDate): Unit =
     withFileReferenceLogged(response) {
       synchronized {
         checkAndRefreshCache(today)
-        responses = responses :+ response
+        responses = response :: responses
       }
       Logger.info(s"Added response: [$response].")
     }
@@ -28,11 +29,10 @@ class InMemoryResponseConsumer(private var responsesDate: LocalDate,
   // NOTE: Can return stale results from a previous day -- iff the day has changed and no new responses have yet been received for the current day.
   override def retrieveResponses: ResponseLog = ResponseLog(responsesDate, responses)
 
-  private def checkAndRefreshCache(today: LocalDate): Unit = {
+  private def checkAndRefreshCache(today: LocalDate): Unit =
     if (today.isAfter(responsesDate)) {
       responsesDate = today
-      responses = Seq.empty
+      responses     = List.empty[JsValue]
       Logger.info(s"Resetting responses for new day: [$today].")
     }
-  }
 }
