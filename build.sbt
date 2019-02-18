22038509cb1894f0d942904122d7dbbe3b478d44
Dario Abdulrehman <167463+dabd@users.noreply.github.com>
2019-02-18 17:07:17 +0000

    PLATOPS-1850: Library security upgrades

--- build.sbt
+++ build.sbt
@@ -28,7 +28,7 @@ lazy val microservice = Project(appName, file("."))
   )
   .settings(PlayKeys.playDefaultPort := 9577)
   .settings(scalaSettings ++ scoverageSettings: _*)
-  .settings(majorVersion := 1)
+  .settings(majorVersion := 2)
   .settings(
     publishingSettings: _*
   )
--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,27 +1,26 @@
 import play.core.PlayVersion
 import play.sbt.PlayImport._
-import sbt.Keys.libraryDependencies
 import sbt._
 
 object AppDependencies {
 
   val compile = Seq(
     ws,
-    "uk.gov.hmrc" %% "bootstrap-play-25" % "3.15.0"
+    "uk.gov.hmrc" %% "bootstrap-play-25" % "4.9.0"
   )
 
   def test(scope: String = "test") = Seq(
     "uk.gov.hmrc" %% "hmrctest" % "3.2.0" % scope,
-    "uk.gov.hmrc"            %% "http-verbs-test"             % "1.2.0"             % scope,
-    "org.scalatest"          %% "scalatest"                   % "2.2.6"             % scope,
+    "uk.gov.hmrc"            %% "http-verbs-test"             % "1.3.0"             % scope,
+    "org.scalatest"          %% "scalatest"                   % "3.0.5"             % scope,
     "org.pegdown"            % "pegdown"                      % "1.6.0"             % scope,
     "com.typesafe.play"      %% "play-test"                   % PlayVersion.current % scope,
     "org.mockito"            % "mockito-core"                 % "2.6.2"             % scope,
     "org.scalamock"          %% "scalamock-scalatest-support" % "3.5.0"             % scope,
-    "org.scalatestplus.play" %% "scalatestplus-play"          % "2.0.0"             % scope,
-    "com.typesafe.play"      %% "play-ws"                     % "2.5.6"             % scope,
+    "org.scalatestplus.play" %% "scalatestplus-play"          % "2.0.1"             % scope,
+    "com.typesafe.play"      %% "play-ws"                     % PlayVersion.current % scope,
     "commons-io"             % "commons-io"                   % "2.6"               % scope,
-    "org.scalatest" %% "scalatest" % "3.0.4" % scope,
+    "org.scalatest" %% "scalatest" % "3.0.5" % scope,
     "org.pegdown" % "pegdown" % "1.6.0" % scope,
     "org.mockito"            % "mockito-core"                 % "2.6.2"             % scope,
     "org.scalamock"          %% "scalamock-scalatest-support" % "3.5.0"             % scope,
