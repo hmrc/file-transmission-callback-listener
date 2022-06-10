678c6867576c70ef96e0534035dd204f51e56023
Shnick <1132919+Shnick@users.noreply.github.com>
2022-06-10 16:01:13 +0100

    BDOG-1627 fix comments raised by PR bot

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -15,13 +15,4 @@ object AppDependencies {
     "uk.gov.hmrc" %% "bootstrap-test-play-28"     % bootstrapVersion   % "test, it",
     "org.mockito" %% "mockito-scala-scalatest"    % "1.17.5"           % Test,
   )
-  //   "org.pegdown"            %  "pegdown"                     % "1.6.0"             % Test,
-  //   "com.typesafe.play"      %% "play-test"                   % PlayVersion.current % Test,
-  //   "org.mockito"            %  "mockito-core"                % "2.6.2"             % Test,
-  //   "org.scalamock"          %% "scalamock-scalatest-support" % "3.5.0"             % Test,
-  //   "org.scalatestplus.play" %% "scalatestplus-play"          % "4.0.0"             % Test,
-  //   "com.typesafe.play"      %% "play-ws"                     % PlayVersion.current % Test,
-  //   "commons-io"             %  "commons-io"                  % "2.6"               % Test,
-  //   "org.scalamock"          %% "scalamock-scalatest-support" % "3.5.0"             % Test
-  // )
 }
